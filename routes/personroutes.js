3b40d5448f8de5c59e830828144348d960884a1f
Ashish <ashishgupta99116@gmail.com>
2026-08-29 20:09:26 +0530

    project done

--- routes/personroutes.js
+++ routes/personroutes.js
@@ -2,8 +2,9 @@ const express = require('express') ;
 const router= express.Router() ;
 
 const Person = require('./../models/Person') ;
+const {jwtAuthMiddleware , generatetoken} = require('./../jwt') ;
 
-router.post('/' , async (req , res) =>{
+router.post('/signup' , async (req , res) =>{
 
     try{
         const data = req.body // assuming the request body constain the person data 
@@ -14,12 +15,51 @@ router.post('/' , async (req , res) =>{
         // save the new person data to the database
         const response = await newPerson.save() ;
         console.log('data saved') ;
-        res.status(200).json(response) ;
+
+        const payload = {
+            id : response.id ,
+            username : response.username
+        }
+
+        console.log(payload);
+        const token = generatetoken(payload) ;
+        console.log("Token id : " , token) ;
+        res.status(200).json({response : response , token : token}) ;
     }
     catch(err){
         console.log(err) ;
         res.status(500).json({error : 'Internal server error'}) ;
     }
+})
+
+
+// login route
+
+router.post('/login' , async ( req, res) => {
+    try{
+        // extract the username and password
+        const {username , password} = req.body ;
+
+        // find the user by username 
+        const user = await Person.findOne({username : username}) ;
+
+        // if user doen not exist or password is incorrect
+        if(!user || !(await user.comparePassword(password))){
+            return res.status(401).json({error : 'Invalid username or password'}) ;
+        }
+
+
+        const payload = {
+            id: user.id ,
+            username: user.username 
+        }
+        const token = generatetoken(payload) ;
+        res.json({token}) ;
+    }catch(err){
+        console.error(err) ;
+        res.status(500).json({error : 'Internal server error'}) ;
+    }
+    
 })
 
 // Get method to get the person data 
@@ -34,5 +74,20 @@ router.get('/' , async(req, res) => {
     }
 })
 
+// for profile
+router.get('/profile' ,jwtAuthMiddleware , async (req , res) => {
+    try{
+        const userData = req.user  ;
+        console.log("User data: " , userData) ;
+
+        const userId = userData.id ;
+        const user = await Person.findById(userId) ;
+
+        res.status(200).json({user}) ;
+    }catch(err){
+        console.error(err) ;
+        res.status(500).json({error : 'INTERNAL SERVER ERROR'}) ;
+    }
+})
 
 module.exports = router ;
--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const personroutes = require('./routes/personroutes');
 const menuroutes = require('./routes/menuroutes');
 
 app.use('/menu', menuroutes);
-app.use('/person' , LocalAuthMiddleware , personroutes);
+app.use('/person' , personroutes);
 
 const PORT = 3000;
 
